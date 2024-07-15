fe9448691f52f7079be245768acecb725dbd63a5
Simon <44496148+siim0n@users.noreply.github.com>
2024-07-15 19:01:19 +0200

    better repo

--- src/generateGrid.ts
+++ src/generateGrid.ts
@@ -1,6 +1,7 @@
 import * as h3 from "h3-js";
 import * as fs from "fs";
 import * as turf from "@turf/turf";
+import clientPromise from "./db/mongoclient";
 
 // Load your GeoJSON file
 const pointsGeoJSON = JSON.parse(fs.readFileSync("miners.geojson", "utf8"));
@@ -20,7 +21,6 @@ function createHexGrid(pointsGeoJSON: any, resolution: number) {
     // Create hexagon geometries
     const hexFeatures = Array.from(hexSet).map((hexId) => {
         const hexBoundary = h3.cellToBoundary(hexId, true);
-        console.log(hexBoundary)
         const hexPolygon = turf.polygon([hexBoundary]);
         return turf.feature(hexPolygon.geometry, { id: hexId });
     });
@@ -30,9 +30,43 @@ function createHexGrid(pointsGeoJSON: any, resolution: number) {
     return hexGeoJSON;
 }
 
+// Function to update miners with hex ID
+const updateMinersWithHexId = async (resolution: number) => {
+    console.log("Updating miners with hex IDs...");
+    const client = await clientPromise;
+    const db = client.db("main");
+    const collection = db.collection("devices");
+
+    // Find miners with a position field
+    console.log("Finding miners with a position field...");
+    const miners = await collection.find({ position: { $exists: true } }).toArray();
+
+    // Update each miner with the hex ID
+    for (const miner of miners) {
+        console.log("hey");
+        const { lng, lat } = miner.position;
+        const hexId = h3.latLngToCell(lat, lng, resolution);
+
+        await collection.updateOne(
+            { _id: miner._id },
+            { $set: { hexId } }
+        );
+    }
+
+};
+
 // Adjust the resolution as needed
 const resolution = 8;
+console.log("Creating hex grid with resolution", resolution);
 const hexGeoJSON = createHexGrid(pointsGeoJSON, resolution);
-console.log(hexGeoJSON)
+console.log(hexGeoJSON);
+
 // Save hex grid to a GeoJSON file
 fs.writeFileSync("hex_grid.geojson", JSON.stringify(hexGeoJSON, null, 2));
+console.log("Hex grid GeoJSON generated successfully.");
+
+// Update miners with hex IDs
+updateMinersWithHexId(resolution).then(() => {
+    console.log("Miners updated with hex IDs successfully.");
+    process.exit(0);
+});
